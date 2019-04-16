d6d077bf22932401ee37b03ec14ec859045d91c2
Steven Nagle <sanagle@ymail.com>
2019-04-15 22:53:49 -0400

    webScraping basics

--- webScraper.py
+++ webScraper.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import requests
+
+with open('example.html') as html_file:
+	soup = BeautifulSoup(html_file, 'lxml')
+
+#print(soup.prettify())
+#match = soup.title.text
+#match = soup.div
+match = soup.find('div', class_='footer')
+print(match)
